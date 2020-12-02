0f1eae489823720ee772e60c3806e0ed67a44178
admin <yangcancai@github.com>
2020-12-02 23:29:18 +0800

    add plan

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # captcha
+
 rust 写的轻量级滑动验证码，提供http接口进行调用
+
+# 开发计划
+
+* 完成基本功能验证码图片生成和校验
+* 提供http api接口
+* 增加验证码图库自动爬取 & 图库自动代码生成
+* 增加Makefile打包脚本
+* 增加配置
+* 增加log日志打印
+* 增加控制台cli
+* 增加接口频率限制
+* 增加接口参数校验
+
+# 接口列表
+
+* 生成验证码接口
+* 校验验证码接口
+
+# 参考链接
+
+* [https://github.com/anji-plus/captcha](https://github.com/anji-plus/captcha)
--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,6 @@
 mod lib;
  use lib::{point};
  use lib::manager;
- fn div(a: i32, b: i32) -> Option<i32>{
-     if b == 0{
-         None
-     }else{
-         Some(a/b)
-     }
- }
  fn main() {
      manager::run();
      // 固定抠图x的坐标
@@ -22,15 +15,11 @@ mod lib;
     let mut slidingblock= image::open("images/jigsaw/slidingBlock/1.png").unwrap();
     // 原生图片
     let mut original= image::open("images/jigsaw/original/bg1.png").unwrap().to_rgba();
-    let mut xx: i32 = 99999; 
     for x in 0..slidingblock.width(){
         for y in 0..slidingblock.height(){
         // 找到不是透明的像素,把原生的像素copy到抠图图片
         let pixel = slidingblock.get_pixel(x, y);
         if pixel[3] > 0 {
-            if (y as i32) < xx {
-                xx = y as i32;
-            }
             // 获取原生图片当前像素值 
             let org_pixel = *original.get_pixel(x + rand_x, y);
             // 把原生像素拷贝到抠图位置上
@@ -44,46 +33,7 @@ mod lib;
         }
         }
     }
-    println!("xx = {}", xx);
     // 写入文件
     slidingblock.save("sliding_1.png").unwrap();
     original.save("org_bg1.png").unwrap();
- }
- fn test_draw(){
-   let mut img2 = image::open("fractal.png").unwrap();
-   let mut img1 = img2.to_rgba();
-   let min_x = img1.width()/2-10;
-   let min_y = img1.height()/4-10;
-   let max_x = img1.width()-min_x;
-   let max_y = img1.height()-min_y;
-    // 画一个十字架
-    for x in min_x..=max_x {
-        for y in min_y..max_y {
-            let pixel = *img1.get_pixel(x, y);
-            img1.put_pixel(x, y, image::Rgba([pixel[0], pixel[1], pixel[2], 200]));
-            let pixel = *img1.get_pixel(y, x);
-            img1.put_pixel(y, x, image::Rgba([pixel[0], pixel[1], pixel[2], 200]));
-        } 
-   }
-  let offx = img1.width() / 2;
-  let offy = img1.height()/2;
-  let r = 100;
-  let mut x = 0;
-  let mut y: i32 = 0;
-  let mut xy = x * x + y * y;
-  let  rr = r * r;
-    // draw(& mut img1,  offx as i32, offy as i32, r);
-    for x in x..r{
-        for y in y..r{
-            if x*x + y*y < rr{
-             point(& mut img1, x , y, offx as i32, offy as i32);
-             point(& mut img1, -x, -y, offx as i32, offy as i32);
-             point(& mut img1, -x, y, offx as i32, offy as i32);
-             point(& mut img1, x, -y, offx as i32, offy as i32);
-            }
-        }
-    }
-    img1.save("new_f.png").unwrap();
-
-
  }
